f914e69c572ded14786599e2c17fc51389a9ec5e
Paulo Solipa <psolipa@product-league.com>
2023-08-25 15:49:55 +0100

    Change to minify html

--- hooks/errorscreen_changer.js
+++ hooks/errorscreen_changer.js
@@ -10,7 +10,7 @@ module.exports = function (context) {
     utils.indexJSChanger(context.opts.projectRoot + confs.androidPath + "scripts/ECOP_Mobile_PS.index.js");
     utils.minifier(context.opts.projectRoot + confs.androidPath + "scripts", '.js', {js: true});
     utils.minifier(context.opts.projectRoot + confs.androidPath + "css", '.css', {} );
-    utils.minifier(context.opts.projectRoot + confs.androidPath, '.html',{html: true});
+    //utils.minifier(context.opts.projectRoot + confs.androidPath, '.html',{html: true});
     utils.minifier(context.opts.projectRoot + confs.androidPath, '.js', {js: true});
     utils.minifyImages(context.opts.projectRoot + confs.androidPath + 'img');
 
