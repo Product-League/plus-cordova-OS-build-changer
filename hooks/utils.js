93f828fccfe13874a8b9b798acb86583c530e44e
Paulo Solipa <psolipa@product-league.com>
2023-08-28 15:15:11 +0100

    Test includes

--- hooks/utils.js
+++ hooks/utils.js
@@ -1,17 +1,17 @@
 const fs = require('fs'),
-path = require('path'),
-minify = require('minify'),
-CleanCSS = require('clean-css'),
-imagemin = require('imagemin'),
-imageminPng = require('imagemin-pngquant'),
-imageminJpeg = require('imagemin-jpegtran'),
-imageminSVG = require('imagemin-svgo'),
-imageminGIF = require('imagemin-gifsicle'),
-xml2js = require('xml2js'),
-cssOptions = {
-    keepSpecialComments: 0
-},
-cssMinifier = new CleanCSS(cssOptions);
+    path = require('path'),
+    minify = require('minify'),
+    CleanCSS = require('clean-css'),
+    imagemin = require('imagemin'),
+    imageminPng = require('imagemin-pngquant'),
+    imageminJpeg = require('imagemin-jpegtran'),
+    imageminSVG = require('imagemin-svgo'),
+    imageminGIF = require('imagemin-gifsicle'),
+    xml2js = require('xml2js'),
+    cssOptions = {
+        keepSpecialComments: 0
+    },
+    cssMinifier = new CleanCSS(cssOptions);
 
 //Initial configs
 const configs = {
@@ -44,27 +44,27 @@ function indexReplacer(path, content) {
     content = content.replace('<script type="text/javascript" src="scripts/OutSystemsManifestLoader.js', '<script async type="text/javascript" src="scripts/OutSystemsManifestLoader.js');
     console.log('OutSystemsManifestLoader async')
     //content = content.replace('<script type="text/javascript" src="scripts/OutSystems.js', '<script async type="text/javascript" src="scripts/OutSystems.js');
-    
+
     content = content.replace('<script type="text/javascript" src="scripts/OutSystemsReactView.js', '<script async type="text/javascript" src="scripts/OutSystemsReactView.js');
     console.log('OutSystemsReactView async')
-    
+
     content = content.replace('<script type="text/javascript" src="scripts/cordova.js', '<script async type="text/javascript" src="scripts/cordova.js');
     console.log('cordova async')
-    
+
     content = content.replace('<script type="text/javascript" src="scripts/Debugger.js', '<script async type="text/javascript" src="scripts/Debugger.js');
     console.log('Debugger async')
-    
+
     content = content.replace('<script type="text/javascript" src="scripts/ECOP_Mobile_PS.appDefinition.js', '<script async type="text/javascript" src="scripts/ECOP_Mobile_PS.appDefinition.js');
     console.log('appDefinition async')
-    
+
     content = content.replace('<script type="text/javascript" src="scripts/OutSystemsReactWidgets.js', '<script async type="text/javascript" src="scripts/OutSystemsReactWidgets.js');
     console.log('OutSystemsReactWidgets async')
-    
+
     content = content.replace('<script type="text/javascript" src="scripts/ECOP_Mobile_PS.index.js', '<script async type="text/javascript" src="scripts/ECOP_Mobile_PS.index.js')
     console.log('index async')
-    
+
     content = content.substr(0, content.indexOf('<script type="text/javascript" src="scripts/NullDebugger.js')) + content.substr(content.indexOf('</script>', content.indexOf('<script type="text/javascript" src="scripts/NullDebugger.js')) + 9)
-    
+
     fs.writeFileSync(path, content, "utf-8");
 }
 
@@ -75,12 +75,12 @@ function indexJSChanger(path) {
     fs.writeFileSync(path, indexjs, 'utf-8');
 }
 
-function minifier (dirPath, fileExtension, options) {
-    fs.readdirSync(dirPath).filter(file => 
+function minifier(dirPath, fileExtension, options) {
+    fs.readdirSync(dirPath).filter(file =>
         file.endsWith(fileExtension)).forEach(file => {
             switch (true) {
                 case fileExtension === '.css':
-                    if(!file.startsWith('PLUS_OutSystemsUI_2_8_0')){
+                    if (!file.startsWith('PLUS_OutSystemsUI_2_8_0')) {
                         console.log("Minifying CSS File: " + file);
                         fs.writeFileSync(path.join(dirPath, file), cssMinifier.minify(fs.readFileSync(path.join(dirPath, file), 'utf-8')).styles);
                     }
@@ -92,20 +92,21 @@ function minifier (dirPath, fileExtension, options) {
                     })
                     break;
                 default:
-                    break;  
+                    break;
             }
         })
 }
 function minifyImages(dirPath) {
     imagemin([dirPath + "**/*.(jpg,svg,gif,png"], {
         cwd: dirPath,
-        destination: dirPath, 
+        destination: dirPath,
         plugins: [
             imageminPng(),
             imageminJpeg(),
             imageminSVG(),
             imageminGIF()
-        ]}).then(() => console.log('Images minified'));
+        ]
+    }).then(() => console.log('Images minified'));
 }
 
 function getAppIdentifier(path) {
@@ -114,30 +115,33 @@ function getAppIdentifier(path) {
 
     parseString(config_xml, (err, config) => {
         if (err) return console.error(err);
-        
-        console.log("App identifier: " +config['widget']['$'].id);
+
+        console.log("App identifier: " + config['widget']['$'].id);
         return config['widget']['$'].id;
     })
 }
 
-function removeUnusedFolders (foldersPath, appId) {
+function removeUnusedFolders(foldersPath, appId) {
     const files = fs.readdirSync(foldersPath);
     files.forEach(folder => {
-        if(!folder.includes(appId + configs.notificareSuffix) || !folder.includes(appId + configs.firebaseSuffix) && folder.includes(configs.firebaseSuffix) && folder.includes(configs.notificareSuffix)) {
-            console.log(folder)
-            const dirFiles = fs.readdirSync(foldersPath + folder);
-            dirFiles.forEach(file => {
-                fs.unlinkSync(foldersPath + folder + "/" + file);
-                console.log(`${file} is deleted!`)
-            })
-            
-            fs.rmdir(foldersPath + folder, err => {
-                if (err) {
-                  throw err;
-                }
-              
-                console.log(`${foldersPath + folder} is deleted!`);
-              });
+        if (folder.includes(configs.notificareSuffix) || folder.includes(configs.firebaseSuffix)) {
+            if (!folder.includes(appId)) {
+                console.log(folder)
+                const dirFiles = fs.readdirSync(foldersPath + folder);
+                dirFiles.forEach(file => {
+                    fs.unlinkSync(foldersPath + folder + "/" + file);
+                    console.log(`${file} is deleted!`)
+                })
+
+                fs.rmdir(foldersPath + folder, err => {
+                    if (err) {
+                        throw err;
+                    }
+
+                    console.log(`${foldersPath + folder} is deleted!`);
+                });
+            }
+
         }
     })
 }
