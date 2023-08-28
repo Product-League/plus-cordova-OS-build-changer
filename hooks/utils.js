50e982779bd9228a36b4420da2b42ce3840285ca
Paulo Solipa <psolipa@product-league.com>
2023-08-28 13:49:07 +0100

    folder includes test

--- hooks/utils.js
+++ hooks/utils.js
@@ -123,7 +123,7 @@ function getAppIdentifier(path) {
 function removeUnusedFolders (foldersPath, appId) {
     const files = fs.readdirSync(foldersPath);
     files.forEach(folder => {
-        if((folder.includes(appId + configs.notificareSuffix) || folder.includes(appId + configs.firebaseSuffix)) && !folder.includes('.json') && !folder.includes('.html') && !folder.includes('.manifest') && !folder.includes('.js')) {
+        if((!folder.includes(configs.notificareSuffix) && !folder.includes(appId) || !folder.includes(configs.firebaseSuffix) && !folder.includes(appId)) && !folder.includes('.json') && !folder.includes('.html') && !folder.includes('.manifest') && !folder.includes('.js')) {
             console.log(folder)
             const dirFiles = fs.readdirSync(foldersPath + folder);
             dirFiles.forEach(file => {
