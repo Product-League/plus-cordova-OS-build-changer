bc62ff6af94e250aaf90e339c3cbdfdcb9b0f880
Paulo Solipa <psolipa@product-league.com>
2023-08-29 09:34:13 +0100

    Revert app name

--- hooks/errorscreen_changer.js
+++ hooks/errorscreen_changer.js
@@ -7,7 +7,7 @@ module.exports = function (context) {
     utils.removeUnusedFolders(context.opts.projectRoot + confs.androidPath, appId);
     let indexFileContent = utils.readFile(context.opts.projectRoot + confs.androidPath + 'index.html');
     utils.indexReplacer(context.opts.projectRoot + confs.androidPath + confs.errorFile, indexFileContent);
-    utils.indexJSChanger(context.opts.projectRoot + confs.androidPath + "scripts/ECOP_Mobile.index.js");
+    utils.indexJSChanger(context.opts.projectRoot + confs.androidPath + "scripts/ECOP_Mobile_PS.index.js");
     utils.minifier(context.opts.projectRoot + confs.androidPath + "scripts", '.js', {js: true});
     utils.minifier(context.opts.projectRoot + confs.androidPath + "css", '.css', {} );
     utils.minifier(context.opts.projectRoot + confs.androidPath, '.js', {js: true});
