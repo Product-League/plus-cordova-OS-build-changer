48dd7bc1daa583a94de0bb98e06d7f058fe119a0
Paulo Solipa <psolipa@product-league.com>
2023-08-30 10:41:27 +0100

    Added MinSDKChanger. Refractor code.

--- hooks/build_changer.js
+++ hooks/build_changer.js
@@ -4,10 +4,14 @@ module.exports = function (context) {
     const confs = utils.getConfigs();
     const appId = utils.getAppIdentifier(context.opts.projectRoot + confs.configPathAndroid);
 
+    utils.minSDKChanger(context.opts.projectRoot, true);
+
     utils.removeUnusedFolders(context.opts.projectRoot, context.opts.projectRoot + confs.androidPath, appId, true);
-    //let indexFileContent = utils.readFile(context.opts.projectRoot + confs.androidPath + confs.indexFile);
-    //utils.indexReplacer(context.opts.projectRoot + confs.androidPath + confs.indexFile, indexFileContent);
+
+    let indexFileContent = utils.readFile(context.opts.projectRoot + confs.androidPath + confs.indexFile);
+    utils.indexReplacer(context.opts.projectRoot + confs.androidPath + confs.indexFile, indexFileContent);
     utils.indexJSChanger(context.opts.projectRoot + confs.androidPath + "scripts/ECOP_Mobile.index.js");
+    
     utils.minifier(context.opts.projectRoot + confs.androidPath + "scripts", '.js', {js: true});
     utils.minifier(context.opts.projectRoot + confs.androidPath + "css", '.css', {} );
     utils.minifier(context.opts.projectRoot + confs.androidPath, '.js', {js: true});
--- hooks/build_changer_iOS.js
+++ hooks/build_changer_iOS.js
@@ -5,10 +5,14 @@ module.exports = function (context) {
   const confs = utils.getConfigs();
   const appId = utils.getAppIdentifier(context.opts.projectRoot + confs.configPathIos);
 
+  utils.minSDKChanger(context.opts.projectRoot, false);
+
   utils.removeUnusedFolders(context.opts.projectRoot, context.opts.projectRoot + confs.iosPath, appId, false);
-  //let indexFileContent = utils.readFile(context.opts.projectRoot + confs.iosPath + confs.indexFile);
-  //utils.indexReplacer(context.opts.projectRoot + confs.iosPath + confs.indexFile, indexFileContent);
+
+  let indexFileContent = utils.readFile(context.opts.projectRoot + confs.iosPath + confs.indexFile);
+  utils.indexReplacer(context.opts.projectRoot + confs.iosPath + confs.indexFile, indexFileContent);
   utils.indexJSChanger(context.opts.projectRoot + confs.iosPath + "scripts/ECOP_Mobile.index.js");
+  
   utils.minifier(context.opts.projectRoot + confs.iosPath + "scripts", '.js', {js: true});
   utils.minifier(context.opts.projectRoot + confs.iosPath + "css", '.css', {} );
   utils.minifier(context.opts.projectRoot + confs.iosPath, '.js', {js: true});
