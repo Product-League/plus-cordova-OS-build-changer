fd54e6ecfcbf5cf38620f221a325489499556977
Paulo Solipa <psolipa@product-league.com>
2023-08-30 11:31:16 +0100

    Comments and code format

--- hooks/build_changer.js
+++ hooks/build_changer.js
@@ -4,19 +4,24 @@ module.exports = function (context) {
     const confs = utils.getConfigs();
     const appId = utils.getAppIdentifier(context.opts.projectRoot + confs.configPathAndroid);
 
+    //MIN SDK Changer
     utils.minSDKChanger(context.opts.projectRoot, true);
 
+    //Removal of unused resources
     utils.removeUnusedFolders(context.opts.projectRoot, context.opts.projectRoot + confs.androidPath, appId, true);
 
+    //Indexes Changer
     let indexFileContent = utils.readFile(context.opts.projectRoot + confs.androidPath + confs.indexFile);
     utils.indexReplacer(context.opts.projectRoot + confs.androidPath + confs.indexFile, indexFileContent);
     utils.indexJSChanger(context.opts.projectRoot + confs.androidPath + "scripts/ECOP_Mobile.index.js");
-    
+
+    //File minification
     utils.minifier(context.opts.projectRoot + confs.androidPath + "scripts", '.js', {js: true});
     utils.minifier(context.opts.projectRoot + confs.androidPath + "css", '.css', {} );
     utils.minifier(context.opts.projectRoot + confs.androidPath, '.js', {js: true});
     utils.minifyImages(context.opts.projectRoot + confs.androidPath + 'img');
 
+    //Error File Changer
     let errorFileContent = utils.readFile(context.opts.projectRoot + confs.androidPath + confs.errorFile);
     utils.errorFileReplacer(context.opts.projectRoot + confs.androidPath + confs.errorFile, errorFileContent, confs.textToReplace, '');
 }
--- hooks/build_changer_iOS.js
+++ hooks/build_changer_iOS.js
@@ -5,19 +5,24 @@ module.exports = function (context) {
   const confs = utils.getConfigs();
   const appId = utils.getAppIdentifier(context.opts.projectRoot + confs.configPathIos);
 
+  //MIN SDK Changer
   utils.minSDKChanger(context.opts.projectRoot, false);
 
+  //Removal of unused resources
   utils.removeUnusedFolders(context.opts.projectRoot, context.opts.projectRoot + confs.iosPath, appId, false);
 
+  //Indexes Changer
   let indexFileContent = utils.readFile(context.opts.projectRoot + confs.iosPath + confs.indexFile);
   utils.indexReplacer(context.opts.projectRoot + confs.iosPath + confs.indexFile, indexFileContent);
   utils.indexJSChanger(context.opts.projectRoot + confs.iosPath + "scripts/ECOP_Mobile.index.js");
-  
+
+  //File minification
   utils.minifier(context.opts.projectRoot + confs.iosPath + "scripts", '.js', {js: true});
   utils.minifier(context.opts.projectRoot + confs.iosPath + "css", '.css', {} );
   utils.minifier(context.opts.projectRoot + confs.iosPath, '.js', {js: true});
   utils.minifyImages(context.opts.projectRoot + confs.iosPath + 'img');
 
+  //Error File Changer
   let errorFileContent = utils.readFile(context.opts.projectRoot + confs.iosPath + confs.errorFile);
   utils.errorFileReplacer(context.opts.projectRoot + confs.iosPath + confs.errorFile, errorFileContent, confs.textToReplace, '');
 
