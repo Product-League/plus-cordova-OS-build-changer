c2287b8871f656fadd342c3e1d3a826ac224b655
Paulo Solipa <psolipa@product-league.com>
2023-08-31 15:38:41 +0100

    test

--- hooks/build_changer.js
+++ hooks/build_changer.js
@@ -5,7 +5,7 @@ module.exports = function (context) {
     const appId = utils.getAppIdentifier(context.opts.projectRoot + confs.configPathAndroid);
 
     //Firebase Performance Logcat Addition
-    utils.performanceLogcatAdd(context.opts.projectRoot + confs.androidMainPath + confs.androidManifest);
+    //utils.performanceLogcatAdd(context.opts.projectRoot + confs.androidMainPath + confs.androidManifest);
 
     //MIN SDK Changer Android
     utils.minSDKChangerAndroid(context.opts.projectRoot);
