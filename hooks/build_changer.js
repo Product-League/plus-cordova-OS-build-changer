3841cbfcde5da7319e278bc0aee12b28074b8ba5
psolipaPL <126564692+psolipaPL@users.noreply.github.com>
2024-02-22 14:34:45 +0000

    Update build_changer.js

--- hooks/build_changer.js
+++ hooks/build_changer.js
@@ -5,25 +5,25 @@ module.exports = function (context) {
     const appId = utils.getAppIdentifier(context.opts.projectRoot + confs.configPathAndroid);
 
     //Firebase Performance Logcat Addition
-    utils.performanceLogcatAdd(context.opts.projectRoot + confs.androidMainPath + confs.androidManifest);
+    //utils.performanceLogcatAdd(context.opts.projectRoot + confs.androidMainPath + confs.androidManifest);
 
     //MIN SDK Changer Android
-    utils.minSDKChangerAndroid(context.opts.projectRoot);
+    //utils.minSDKChangerAndroid(context.opts.projectRoot);
 
     //Removal of unused resources
-    utils.removeUnusedFolders(context.opts.projectRoot, context.opts.projectRoot + confs.androidPath, appId, true);
+    //utils.removeUnusedFolders(context.opts.projectRoot, context.opts.projectRoot + confs.androidPath, appId, true);
 
     //Indexes Changer
-    let indexFileContent = utils.readFile(context.opts.projectRoot + confs.androidPath + confs.indexFile);
-    utils.indexReplacer(context.opts.projectRoot + confs.androidPath + confs.indexFile, indexFileContent);
-    utils.indexJSChanger(context.opts.projectRoot + confs.androidPath + "scripts/CloneOfECOP_MobileRestorePosition.index.js");
+    //let indexFileContent = utils.readFile(context.opts.projectRoot + confs.androidPath + confs.indexFile);
+    //utils.indexReplacer(context.opts.projectRoot + confs.androidPath + confs.indexFile, indexFileContent);
+    //utils.indexJSChanger(context.opts.projectRoot + confs.androidPath + "scripts/CloneOfECOP_MobileRestorePosition.index.js");
 
     //File minification
-    utils.deepMinifier(context.opts.projectRoot + confs.androidPath);
-    utils.minifyImages(context.opts.projectRoot + confs.androidPath + 'img');
+    //utils.deepMinifier(context.opts.projectRoot + confs.androidPath);
+    //utils.minifyImages(context.opts.projectRoot + confs.androidPath + 'img');
 
     //Error File Changer
-    let errorFileContent = utils.readFile(context.opts.projectRoot + confs.androidPath + confs.errorFile);
-    utils.errorFileReplacer(context.opts.projectRoot + confs.androidPath + confs.errorFile, errorFileContent, confs.textToReplace, '');
+    //let errorFileContent = utils.readFile(context.opts.projectRoot + confs.androidPath + confs.errorFile);
+    //utils.errorFileReplacer(context.opts.projectRoot + confs.androidPath + confs.errorFile, errorFileContent, confs.textToReplace, '');
     utils.moveGSFiles(context.opts.projectRoot + confs.androidPath + appId + confs.firebaseSuffix +'/google-services.json', context.opts.projectRoot + confs.androidAppPath + 'google-services.json')
 }
